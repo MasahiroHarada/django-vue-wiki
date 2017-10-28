cb537b105fdbf8a3c88518e99f6a2baa264d4594
Masahiro Harada <masahiro.harada@ari-jp.com>
2017-10-29 00:35:58 +0900

    Change title.

--- frontend/poi.config.js
+++ frontend/poi.config.js
@@ -2,6 +2,7 @@ module.exports = {
   entry: './src/index.js',
   html: {
     inject: false,
+    title: 'Wiki',
     filename: '../../templates/index.html',
     template: './index.ejs',
     appMountId: 'app'
